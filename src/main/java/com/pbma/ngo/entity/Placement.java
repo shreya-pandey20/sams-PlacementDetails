3235f6ec6a720a3673020584b1842bbdb331217b
shreya-pandey20 <shreyapan767@gmail.com>
2023-06-29 20:33:41 +0530

    commit

--- src/main/java/com/pbma/ngo/entity/Placement.java
+++ src/main/java/com/pbma/ngo/entity/Placement.java
@@ -3,19 +3,14 @@ package com.pbma.ngo.entity;
 import java.sql.Date;
 import java.sql.Timestamp;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
